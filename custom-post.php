e9d4cd555ddd41de0dde684e8d473a90f56f36b8
rohanwisdmlabs <rohan.ray@wisdmlabs.com>
2023-04-15 13:37:11 +0530

    created the custom post types

--- custom-post.php
+++ custom-post.php
@@ -73,6 +73,116 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-custom-post.php';
  *
  * @since    1.0.0
  */
+
+function create_custom_post_type()
+{
+	$supports = array(
+'title', // post title
+'editor', // post content
+'author', // post author
+'thumbnail', // featured images
+'excerpt', // post excerpt
+'custom-fields', // custom fields
+'comments', // post comments
+'revisions', // post revisions
+'post-formats', // post formats
+);
+ // sessions label
+$labels = array(
+'name' => _x('Sessions', 'plural'),
+'singular_name' => _x('Session', 'singular'),
+'menu_name' => _x('Sessions', 'admin menu'),
+'name_admin_bar' => _x('Sessions', 'admin bar'),
+'add_new' => _x('Add Session', 'add new'),
+'add_new_item' => __('Add New Session'),
+'new_item' => __('New Session'),
+'edit_item' => __('Edit Session'),
+'view_item' => __('View Session'),
+'all_items' => __('All Session'),
+'search_items' => __('Search Session'),
+'not_found' => __('No Session found.'),
+);
+
+//Courses label
+$courselabels = array(
+	'name' => _x('Coursesss', 'plural'),
+	'singular_name' => _x('Coursess', 'singular'),
+	'menu_name' => _x('Coursesss', 'admin menu'),
+	'name_admin_bar' => _x('Coursesss', 'admin bar'),
+	'add_new' => _x('Add Coursess', 'add new'),
+	'add_new_item' => __('Add New Course'),
+	'new_item' => __('New Course'),
+	'edit_item' => __('Edit Coursess'),
+	'view_item' => __('View Coursess'),
+	'all_items' => __('All Coursess'),
+	'search_items' => __('Search Coursess'),
+	'not_found' => __('No Coursess found.'),
+	);
+
+	//Session args
+ 
+$args = array(
+'supports' => $supports,
+'labels' => $labels,
+'description' => 'Holds our Sessions and specific data',
+'public' => true,
+'taxonomies' => array( 'category', 'post_tag' ),
+'show_ui' => true,
+'show_in_menu' => true,
+'show_in_nav_menus' => true,
+'show_in_admin_bar' => true,
+'can_export' => true,
+'capability_type' => 'post',
+ 'show_in_rest' => true,
+'query_var' => true,
+'rewrite' => array('slug' => 'session'),
+'has_archive' => true,
+'hierarchical' => false,
+'menu_position' => 6,
+'menu_icon' => 'dashicons-book',
+);
+
+//course args
+ 
+$courseargs = array(
+	'supports' => $supports,
+	'labels' => $courselabels,
+	'description' => 'Holds our Courses and specific data',
+	'public' => true,
+	'taxonomies' => array( 'category', 'post_tag' ),
+	'show_ui' => true,
+	'show_in_menu' => true,
+	'show_in_nav_menus' => true,
+	'show_in_admin_bar' => true,
+	'can_export' => true,
+	'capability_type' => 'post',
+	 'show_in_rest' => true,
+	'query_var' => true,
+	'rewrite' => array('slug' => 'coursess'),
+	'has_archive' => true,
+	'hierarchical' => false,
+	'menu_position' => 7,
+	'menu_icon' => 'dashicons-book',
+	);
+ 
+register_post_type('session', $args);
+register_post_type('coursess', $courseargs);
+flush_rewrite_rules( false );
+}
+add_action('init', 'create_custom_post_type');
+
+
+
+
+
+
+
+
+
+
+
+
+
 function run_custom_post() {
 
 	$plugin = new Custom_Post();
